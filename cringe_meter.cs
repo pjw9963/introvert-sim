1067353b0bbd334656f6822da0228410bd502c8d
Ryan Shu <rrs6624@g.rit.edu>
2024-01-26 20:27:46 -0500

    you've cringed

--- cringe_meter.cs
+++ cringe_meter.cs
@@ -3,24 +3,29 @@ using System;
 
 public partial class cringe_meter : Node2D
 {
+	[Export]
+	public int currentCringePoints = 0;
+	protected bool cringed = false;
 	
-	public class cringeMeter(int maxCringePoints = 5){
+	[Export]
+	int maxCringePoints = 5;
+		
+	public cringe_meter(int maxCringePoints)
+	{
 		this.maxCringePoints = maxCringePoints;
-		
-		[Export]
-		public int currentCringePoints = 0;
-		protected bool cringed = false;
-		
-		private int damage(int damageTaken){
+	}
+	protected int damage(int damageTaken){
 			currentCringePoints += damageTaken;
 			Console.WriteLine("You've taken " + damageTaken + " points of damage.");
+			return currentCringePoints;
 		}
 		
-		private int loss() {
+	protected bool loss() {
 			if (maxCringePoints == currentCringePoints){
 				cringed = true;
-				Console.WriteLien("Youve cringed!");
+				Console.WriteLine("Youve cringed!");
+				return cringed;
 			}
+			return cringed;
 		}
-	}
 }
--- take_damage.cs
+++ take_damage.cs
@@ -6,6 +6,7 @@ public partial class take_damage : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		cringe_meter TestHp = new cringe_meter(2);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
